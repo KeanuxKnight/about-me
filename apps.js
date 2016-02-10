d5731becca80e192d54ff88de34b201a19a68277
KeanuXKnight <keanuroush@gmail.com>
2016-02-10 13:06:08 -0800

    The Bulk of the script

--- apps.js
+++ apps.js
@@ -21,4 +21,66 @@ if (startgame == 'C') {
   location.reload();
 }
 
-//questions start here
+//presets
+var cScore = 0;
+var iScore = 0;
+console.log('score:');
+
+//questions start here @ Q1
+var q1 = prompt('let us start off easy. What is my middle name? (Mouse over my name.)');
+if (q1.toUpperCase() == 'MICHEAL') {
+  alert('Good. That was easy, right.');
+  var cScore = ++cScore;
+}else {
+  alert('Nope. Try harder next time.');
+  var iScore = ++iScore;
+}
+console.log('S=' + cScore + ', ' + iScore);
+// Q2 --------------------------------------------
+var q2 = prompt('How old am I?');
+if (q2 == 22) {
+  alert('Good. That was easy, right.');
+  var cScore = ++cScore;
+}else {
+  alert('Nope. Try harder next time.');
+  var iScore = ++iScore;
+}
+console.log('S=' + cScore + ', ' + iScore);
+
+// Q3 ---------------------------------------------
+var q3 = prompt('how long did I do Ballet');
+if (q3 == 7) {
+  alert('Good.');
+  var cScore = ++cScore;
+}else {
+  alert('Nope. Try harder next time.');
+  var iScore = ++iScore;
+}
+console.log('S=' + cScore + ', ' + iScore);
+
+// Q4 --------------------------------------------
+var q4 = prompt('Did I ever Learn Karate?  Y/N');
+if (q4.toUpperCase() == 'N') {
+  alert('Good.');
+ var cScore = ++cScore;
+}else {
+  alert('Nope. Try harder next time.');
+ var iScore = ++iScore;
+}
+console.log('S=' + cScore + ', ' + iScore);
+
+//Q5----------------------------------------------
+var q4 = prompt('Do I have 5 siblings?  Y/N');
+if (q4.toUpperCase() == 'N') {
+  alert('Good. They are all half siblings.');
+  var cScore = ++cScore;
+}else {
+  alert('Nope. They are all half siblings, but because this was a trick question, I\'ll give you the point.');
+ var iScore = ++cScore;
+}
+console.log('S=' + cScore + ', ' + iScore);
+
+//  The  End -----------------------------------------------
+alert('Your score was')
+alert(cScore + 'correct & ' + iScore + 'incorrect')
+alert('your total is ' + (cScore - iScore) )
